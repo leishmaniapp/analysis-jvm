4ef4bbceae0ef4dee79ce17015d85f26c4b8f089
Ángel Talero <angelgotalero@outlook.com>
2024-08-05 22:03:31 -0500

    Fixed missing maven-publish on root build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,6 @@
 plugins {
+    `maven-publish`
+
     alias(libs.plugins.android.library) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.parcelize) apply false
@@ -6,5 +8,5 @@ plugins {
 
 allprojects {
     group = "com.leishmaniapp.analysis"
-    version = "1.1.0"
+    version = "1.1.1"
 }
